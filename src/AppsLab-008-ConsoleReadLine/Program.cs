60000cc317a995f746bc94e85cd929e1f40ddea9
patrik <smrzo@ASUS_PATRIK>
2025-09-24 13:02:04 +0200

    doplnil som otazku

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -5,4 +5,10 @@ Console.WriteLine("Ahoj, " + meno);
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("Aha mas teda " + vek + " rokov.");
+
+Console.WriteLine("Ake je tvoje oblubene jedlo");
+ string food = Console.ReadLine();
+Console.WriteLine("Tvoje oblubene jedlo je " + food);
+
+
