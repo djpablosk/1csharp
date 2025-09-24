1acc30e34d4cd0cd5108c2ce35c89a4bda5cd662
patrik <smrzo@ASUS_PATRIK>
2025-09-24 09:26:39 +0200

    vypisal som datum,vypisal som slovo,vypisal som sucet a splnil ulohy 5 a 6

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int mojeCislo = 10;
+string mojeSlovo = "Ahoj Svet";
+bool mojaBoolhoHodnota = true;
+DateTime dnesnydatum = DateTime.Now;
+float myfloat = 20.54f;
+var myvar = ("var string ide");
+
+int sucet = mojeCislo + 5;
+
+Console.WriteLine($"Súčet je: {sucet}");
+Console.WriteLine($"Dnes je: {dnesnydatum.ToString("yyyy-MM-dd")}");
+Console.WriteLine($"Moje slovo je: {mojeSlovo}");
+Console.WriteLine($"Moja bool hodnota je: {mojaBoolhoHodnota}");
+Console.WriteLine("10");
+Console.WriteLine(myfloat);
+Console.WriteLine(myvar);
+
+
+
