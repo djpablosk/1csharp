973bc588e159c7653e5afed82d3aaf462283db6b
patrik <smrzo@ASUS_PATRIK>
2025-09-30 16:23:57 +0200

    urobil som bonusovu ulohu a pridal som este dalsie otazky

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -11,4 +11,12 @@ Console.WriteLine("Ake je tvoje oblubene jedlo");
  string food = Console.ReadLine();
 Console.WriteLine("Tvoje oblubene jedlo je " + food);
 
+Console.WriteLine("Aká je tvoja výška v metroch?");
+string vyskaText = Console.ReadLine() ?? "0";
+Console.WriteLine("Ok tvoja výška je " + vyskaText + " metrov.");
+
+Console.WriteLgine("Stlac klavesu ENTER pre ukoncenie");
+var key = Console.ReadKey ();
+Console.WriteLine("Stlacena klavesa je " + key.KeyChar);
+
 
